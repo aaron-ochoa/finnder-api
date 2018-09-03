903eb57c65c9c78be89e622cac9f6482437e49d5
Aaron Ochoa <ochoaa@PA013589MAC.local>
2018-09-03 12:21:22 -0700

    Created resturants endpoints

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import jsonify
+from yelp_service import YelpService
+from flask import request
 
 app = Flask(__name__)
+
+yelp = YelpService()
 
 def respond(message, code, result=None):
     return jsonify({
@@ -17,5 +21,9 @@ def ping():
 
 @app.route('/api/v1/restaurants')
 def get_restaurants():
-    restaurants = []
+    lat = request.args['latitude']
+    long = request.args['longitude']
+    limit = request.args['limit']
+    offset = request.args['offset']
+    restaurants = yelp.get_restaurants(lat, long, limit, offset)
     return respond("Successful got restaurants", 2000, restaurants)
---
+++ yelp_service.py
@@ -0,0 +1,45 @@
+#service
+import requests
+from os import environ as env
+
+class YelpService:
+
+    url = "https://api.yelp.com/v3"
+
+    def __init__(self):
+        self.client_id = env.get("YELP_CLIENT_ID")
+        self.secret = env.get("YELP_SECRET")
+
+    def get_restaurants(self, lat, long, limit, offset):
+        querystring = {
+            "latitude":lat,
+            "longitude":long,
+            "categories":"restaurants",
+            "limit": limit,
+            "offset": offset
+        }
+
+
+        response = requests.request("GET", "{}/businesses/search".format(self.url), headers=self._get_headers(), params=querystring)
+
+        restaurants = response.json()["businesses"]
+        results = []
+
+        for restaurant in restaurants:
+            details = self._get_restaurant_details(restaurant["id"])
+            restaurant["photos"] = details["photos"]
+            results.append(restaurant)
+
+        return results
+
+    def _get_restaurant_details(self, id):
+
+        response = requests.request("GET", "{}/businesses/{}".format(self.url, id), headers=self._get_headers())
+
+        return response.json()
+
+    def _get_headers(self):
+
+        return {
+            'Authorization': "Bearer {}".format(self.secret)
+        }
